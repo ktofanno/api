2a6a4bfa020b98dada55ec03040d8c828d8e9172
Marcos S. Ferreira <marcos.ferreira@gmail.com>
2025-10-26 22:01:27 -0300

    Tratamento de exceções

--- src/main/java/br/senac/rj/api/controller/LivroController.java
+++ src/main/java/br/senac/rj/api/controller/LivroController.java
@@ -1,7 +1,11 @@
 package br.senac.rj.api.controller;
 
+import br.senac.rj.api.exceptions.ResourceNotFoundException;
 import br.senac.rj.api.model.Livro;
 import br.senac.rj.api.service.LivroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,13 +19,52 @@ public class LivroController {
     public LivroController(LivroService livroService) {
         this.livroService = livroService;
     }
+
     @GetMapping("/livros")
-    public List<Livro> listarLivros() {
-        return this.livroService.listarLivros();
+    public ResponseEntity<?> listarLivros() {
+        try {
+            List<Livro> livros = this.livroService.listarLivros();
+            return ResponseEntity.ok(livros);
+        } catch (ResourceNotFoundException rnfe) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(rnfe.getMessage());
+        }
     }
 
     @PostMapping("/livros")
-    public Livro incluirLivro(@RequestBody Livro livro) {
-        return this.livroService.incluirLivro(livro);
+    public ResponseEntity<Livro> incluirLivro(@RequestBody Livro livro) {
+        Livro livroCriado = this.livroService.incluirLivro(livro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroCriado);
+    }
+
+    @GetMapping("/livros/{codigo}")
+    public ResponseEntity<?> buscarLivro(@PathVariable Long codigo) {
+        try {
+            Livro livro = this.livroService.buscarLivroPorCodigo(codigo);
+            return ResponseEntity.ok(livro);
+        } catch (ResourceNotFoundException rnfe) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(rnfe.getMessage());
+        }
+    }
+
+    @DeleteMapping("/livros/{codigo}")
+    public ResponseEntity<?> excluirLivro(@PathVariable Long codigo) {
+        try {
+            this.livroService.excluirLivro(codigo);
+            return ResponseEntity.noContent().build();
+        } catch (ResourceNotFoundException rnfe) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(rnfe.getMessage());
+        }
+
+    }
+
+    @PutMapping("/livros/{codigo}")
+    public ResponseEntity<?> excluirLivro(@PathVariable Long codigo, @RequestBody Livro livro) {
+        try {
+            Livro livroAtualizado = this.livroService.atualizarLivro(codigo, livro);
+            return ResponseEntity.ok(livroAtualizado);
+        } catch (ResourceNotFoundException rnfe) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(rnfe.getMessage());
+        }
+
     }
 }
--- src/main/java/br/senac/rj/api/service/LivroService.java
+++ src/main/java/br/senac/rj/api/service/LivroService.java
@@ -1,11 +1,11 @@
 package br.senac.rj.api.service;
 
+import br.senac.rj.api.exceptions.ResourceNotFoundException;
 import br.senac.rj.api.model.Livro;
 import br.senac.rj.api.repository.LivroRepository;
 import org.springframework.stereotype.Service;
 
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +18,10 @@ public class LivroService {
     }
 
     public List<Livro> listarLivros() {
+        List<Livro> livros = livroRepository.findAll();
+        if (livros.isEmpty()) {
+            throw new ResourceNotFoundException("Não há livros cadastados");
+        }
         return this.livroRepository.findAll();
     }
 
@@ -25,16 +29,31 @@ public class LivroService {
         return this.livroRepository.save(livro);
     }
 
-    public Optional<Livro> buscarLivroPorCodigo(Long codigo) {
-        return this.livroRepository.findById(codigo);
+    // remover o <Optional> => public Optional<Livro> buscarLivroPorCodigo(Long codigo) {
+    public Livro buscarLivroPorCodigo(Long codigo) {
+        String message = "Livro com codigo [" + codigo + "] não encontrado";
+        return this.livroRepository.findById(codigo).orElseThrow(() -> new ResourceNotFoundException(message));
     }
 
     public void excluirLivro(Long codigo) {
+        if (! this.livroRepository.existsById(codigo)) {
+            throw new ResourceNotFoundException("Livro com codigo [" + codigo + "] não encontrado");
+        }
         this.livroRepository.deleteById(codigo);
     }
 
     public Livro atualizarLivro(Long codigo, Livro livroAtualizado) {
-        return null;
+        Optional<Livro> optionalLivro = this.livroRepository.findById(codigo);
+
+        if (optionalLivro.isEmpty()) {
+            throw new ResourceNotFoundException("Livro com codigo [" + codigo + "] não encontrado");
+        } else {
+            Livro livro = optionalLivro.get();
+            livro.setTitulo(livroAtualizado.getTitulo());
+            livro.setPreco(livroAtualizado.getPreco());
+
+            return this.livroRepository.save(livro);
+        }
     }
 
 }
